3176702a25523117d0ee37662fd3bfb73fc8f805
Camilo Andres Rodriguez <andres.roh@gmail.com>
2024-05-10 12:17:03 -0500

    Initial commit, create object session

---
+++ main.py
@@ -0,0 +1,14 @@
+from MeliAPIClient import Session
+from MeliAPIClient import Order
+
+
+def run():
+
+    session = Session("ml_keys.json", 'ml_token.json', "ml_user.json")
+    orders = Order(session)
+
+    orders.list(year=2024, month=5)
+
+
+if __name__ == "__main__":
+    run()
